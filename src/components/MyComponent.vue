2f01ef4c852f2ca9a0923a7c38d625d8c7f30834
TorMagne <tormagne-brekken@outlook.com>
2023-03-01 14:50:05 +0100

    .

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- your component template here -->
+  <h1>{{ message }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('This is a compiled vue component');
+</script>
+
+<style scoped>
+/* your component styles here */
+</style>
